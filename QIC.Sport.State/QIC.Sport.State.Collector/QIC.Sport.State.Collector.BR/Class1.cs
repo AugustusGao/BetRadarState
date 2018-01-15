702b1202a56758bc6d7efa42d8c4af81ed9cd4a0
Yeasaan <yeasaan8421@gmail.com>
2018-01-15 18:04:42 +0800

    Add BR

---
+++ QIC.Sport.State/QIC.Sport.State.Collector/QIC.Sport.State.Collector.BR/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QIC.Sport.State.Collector.BR
+{
+    public class Class1
+    {
+    }
+}
--- QIC.Sport.State/QIC.Sport.State.Collector/QIC.Sport.State.Collector/Form1.cs
+++ QIC.Sport.State/QIC.Sport.State.Collector/QIC.Sport.State.Collector/Form1.cs
@@ -12,6 +12,7 @@ namespace QIC.Sport.State.Collector
 {
     public partial class Form1 : Form
     {
+        private static int MaxLines = 15;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,18 @@ namespace QIC.Sport.State.Collector
         private void btnStart_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void ShowInfo(string info)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                if (richTextBox1.Lines.Length >= MaxLines)
+                {
+                    richTextBox1.Clear();
+                }
+                richTextBox1.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff  " + info));
+            });
         }
     }
 }
