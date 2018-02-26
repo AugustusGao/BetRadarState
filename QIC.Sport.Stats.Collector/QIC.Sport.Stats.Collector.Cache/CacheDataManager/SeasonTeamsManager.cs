c0a1ea71ff9aee601e6a6e6313f395c92ecbfed6
Yeasaan <yeasaan8421@gmail.com>
2018-02-26 18:27:14 +0800

    Add SeasonTeamsManager.cs

---
+++ QIC.Sport.Stats.Collector/QIC.Sport.Stats.Collector.Cache/CacheDataManager/SeasonTeamsManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QIC.Sport.Stats.Collector.Cache.CacheDataManager
+{
+    public class SeasonTeamsManager : BaseCacheManager
+    {
+
+    }
+}
